c28a76c2e27296b5003f8f110bbbd79a28d1a074
Federico Resano <123256603+fede1525@users.noreply.github.com>
2023-12-08 23:16:59 +0000

    commit

--- exercises/13-Todo-List/index.js
+++ exercises/13-Todo-List/index.js
@@ -3,19 +3,17 @@ const textInput = document.getElementById("addToDo");
 
 
 function deleteTask(event) {
-    let clickedItem = event.target;
-
-    if (clickedItem.classList.contains("fa-trash")) {
-        let listItem = clickedItem.parentElement.parentElement;
-        list.removeChild(listItem);
+    const clickedItem = event.target;
+    if (clickedItem.matches(".fa-trash")) {
+        clickedItem.parentElement.parentElement.remove();
     }
 }
 
 function addTask() {
-    let text = textInput.value.trim();
+    const text = textInput.value.trim();
 
     if (text !== "") {
-        let listItem = document.createElement("li");
+        const listItem = document.createElement("li");
         listItem.innerHTML = `<span><i class="fa fa-trash"></i></span> ${text}`;
         list.appendChild(listItem);
         textInput.value = "";
